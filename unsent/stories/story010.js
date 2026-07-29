f8c01f08d1ff0d5a29e791b3c29bf80ddfc42f17
ABasithN <basithnoor@gmail.com>
2026-07-29 11:57:44 +0530

    Add files via upload

--- unsent/stories/story010.js
+++ unsent/stories/story010.js
@@ -2,18 +2,18 @@ window.STORIES.push({
   contact: "Unknown Number",
 
   history: [
-     {when: "Sun, 25 Feb" }
+     {when: "Sun, 25 Feb"}
 
 ],
 
   existing: [
-    { from: "them", text: "Who is this?" }
+    { from: "them", text: "Got a call. Who is this?" }
   ],
 
   draft: [
     { text: "Someone who couldn't forget your number.", pause: "long" },
     { text: "", pause: "medium" },
-    { text: "Sorry, wrong chat.", pause: "short" }
+    { text: "Sorry, wrong number.", pause: "short" }
   ],
 
   sent: true
